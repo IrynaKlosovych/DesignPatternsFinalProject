70c162300b7dcba1a02cc53bca79d24eed18a5dd
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2024-05-29 22:16:49 +0300

    add classes for Account

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,7 +1,38 @@
-﻿System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+﻿using System.Net.NetworkInformation;
+
+System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
 customCulture.NumberFormat.NumberDecimalSeparator = ".";
 System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 Console.InputEncoding = System.Text.Encoding.Unicode;
 
 
+Console.WriteLine("Вітаємо на головній сторінці!");
+Console.WriteLine("Виберіть пункт для входу, щоби мати можливість здійснювати прості банківські операції:");
+Console.WriteLine("1 - реєстрація в застосунку, 2 - автентифікація");
+string? choosingNumber = Console.ReadLine();
+bool isValidInput = false;
+do
+{
+    if(choosingNumber==null)
+    {
+        Console.WriteLine("Ви не ввели відповідь");
+    }
+    else
+    {
+        switch (choosingNumber)
+        {
+            case "1":
+
+                isValidInput = true;
+                break;
+            case "2":
+
+                isValidInput = true;
+                break;
+            default:
+                Console.WriteLine("Невірний вибір. Будь ласка, виберіть 1 або 2.");
+                break;
+        }
+    }
+} while (!isValidInput);
---
+++ Library/Acounts/User.cs
@@ -0,0 +1,34 @@
+﻿using Library.DB;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Acounts
+{
+    public class User
+    {
+        public string Name {  get; private set; }
+        public string Surname { get; private set; }
+        public string PhoneNumber {  get; private set; }
+
+        public User(string name, string surname, string phoneNumber)
+        {
+            Name = name;
+            Surname = surname;
+            PhoneNumber = phoneNumber;
+        }
+
+        public static User Registry(DataBase instance)
+        {
+
+        }
+
+        public static User Authentication(DataBase instance)
+        {
+
+        }
+    }
+}
