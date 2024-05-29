9fe50c8f314de8079ff1a31ea3e1dbe97d2a1301
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2024-05-29 21:07:46 +0300

    somesthing for db

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,7 +1,9 @@
-﻿System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+﻿using Library.DB;
+
+System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
 customCulture.NumberFormat.NumberDecimalSeparator = ".";
 System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 Console.InputEncoding = System.Text.Encoding.Unicode;
 
-
+DataBase database = DataBase.GetInstance();
--- Library/DB/DataBase.cs
+++ Library/DB/DataBase.cs
@@ -1,13 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library.DB
 {
-    sealed public class DataBase
+    public sealed class DataBase
     {
+        private static DataBase? _instance;
+        private static object _refObj = new object();
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private SqlConnection connection;
 
+        public static DataBase GetInstance()
+        {
+            if (DataBase._instance == null)
+            {
+                lock (DataBase._refObj)
+                {
+                    if (DataBase._instance == null)
+                    {
+                        DataBase._instance = new DataBase();
+                    }
+                }
+            }
+            return DataBase._instance;
+        }
+
+        private DataBase()
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
     }
 }
